afbc0d4d3b6f9100a980f9fab97f9bbc98aeef18
Jose Romualdo Bibera <climbervagabond@gmail.com>
2026-09-04 01:37:03 +0800

    add create and display task

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,32 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 function App() {
+  const [tasks, setTasks] = useState([]);
   const [task, setTask] = useState("");
+
+  function addTask() {
+    setTasks([...tasks, task]);
+    setTask("");
+  }
+
   return (
     <main>
       <h1> Todo App </h1>
       <p>Create a task: {task}</p>
       <input
         value={task}
-        onChange={ (event) => setTask(event.target.value)}
+        onChange={(event) => setTask(event.target.value)}
         placeholder="Enter a task"
       />
+      <br></br>
+
+      <button onClick={addTask}> Add Task </button>
+
+      <br></br>
+
+      {tasks.map((task) => (
+        <h1> {task} </h1>
+      ))}
     </main>
   );
 }
